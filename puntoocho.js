0cf70e8047543078f3ed031a6ad0e8c71b21129d
Andrea <guerreroaanathalia@gmail.com>
2023-09-01 06:21:20 -0500

    PA 31/08/2023 se realiza punto ocho

--- puntoocho.js
+++ puntoocho.js
@@ -15,27 +15,108 @@ let planetaUno = {
   latitud: 80.5,
   longitud: 109.1,
   nivelOxigeno: 0.21,
-  volumenAgua: 0.75,
+  volumenAgua: 0,
 };
 let planetaDos = {
   nombre: "Namek",
   latitud: 180.5,
   longitud: 19.1,
+  nivelOxigeno: -0.8,
+  volumenAgua: 0.09,
+};
+let planetatres = {
+  nombre: "marte",
+  latitud: 280.5,
+  longitud: 20.1,
+  nivelOxigeno: 1.8,
+  volumenAgua: 0.10,
+};
+let planetaCuatro = {
+  nombre: "Mercurio",
+  latitud: 280.5,
+  longitud: 19.1,
+  nivelOxigeno: 0.8,
+  volumenAgua: 0,
+};
+let planetaCinco = {
+  nombre: "Venus",
+  latitud: 380.5,
+  longitud: 29.1,
+  nivelOxigeno: -0.9,
+  volumenAgua: 0.07,
+};
+let planetaSeis = {
+  nombre: "Jupiter",
+  latitud: 380.5,
+  longitud: 19.1,
   nivelOxigeno: 0.8,
   volumenAgua: 0.09,
 };
+let planetaSiete = {
+  nombre: "Urano",
+  latitud: 480.5,
+  longitud: 19.1,
+  nivelOxigeno: 0.8,
+  volumenAgua: 0,
+};
+let planetaOcho = {
+  nombre: "Neptuno",
+  latitud: 580.5,
+  longitud: 19.1,
+  nivelOxigeno: 0.8,
+  volumenAgua: 0.09,
+};
+let planetaNueve = {
+  nombre: "Saturno",
+  latitud: 170.5,
+  longitud: 19.1,
+  nivelOxigeno: 0.8,
+  volumenAgua: 0.09,
+};
+let planetaDiez = {
+  nombre: "Tierra2",
+  latitud: 180.5,
+  longitud: 19.1,
+  nivelOxigeno: 0.1,
+  volumenAgua: 0.19,
+};
 
-let planetas = Array(planetaUno, planetaDos);
+let planetas = Array(planetaUno, planetaDos,planetatres,planetaCuatro,planetaCinco,planetaSeis,planetaSiete,planetaOcho,planetaNueve,planetaDiez);
 
-function analizarPlanetas(planetas) {
+// • Sumar la cantidad total de agua de los 15 planetas
+
+function sumaVolumenAgua(planetas) {
   //recorrer un arreglo
-  let mapa = planetas.map(function (planeta) {
-    return planeta.volumenAgua;
-  });
+  let mapa = planetas.map(planeta => planeta.volumenAgua  );
   let suma = 0;
-  mapa.forEach(function (volumenAgua) {
-    suma = suma+volumenAgua
-  });
-  return suma;
+  mapa.forEach(volumenAgua => suma = suma+volumenAgua); return suma;
 }
-console.log(analizarPlanetas(planetas));
+console.log(`El total del volumen de agua de los planetas es de: `,sumaVolumenAgua(planetas).toFixed(2));
+
+// • Sumar y multiplicar por 100 el total de oxigeno de los 15 planetas
+
+function sumaOxigeno(planetas) {
+  //recorrer un arreglo
+  let mapa = planetas.map(planeta => planeta.nivelOxigeno);
+  let suma = 0;
+  mapa.forEach(oxigeno => suma = suma+oxigeno); return suma
+}
+console.log(`El total del Oxigeno de los planetas es de: `,sumaOxigeno(planetas).toFixed(2),`y este multiplicado por 100 es: `,sumaOxigeno(planetas)*100);
+
+// • Encontrar si alguno de los 15 planetas tiene un nivel de oxigeno
+// negativo y de ser así mostrar la información general de este
+
+function oxigenoNegativo(planetas) {
+  //recorrer un arreglo
+  let oxigenoNegat = planetas.filter(planeta => planeta.nivelOxigeno<0 ); return oxigenoNegat
+}
+console.log(`Los planetas con oxigeno negativo son: `,oxigenoNegativo(planetas));
+
+// • Encontrar si alguno de los 15 planetas no tiene agua y mostrar la
+// información general de este
+
+function sinAgua(planetas) {
+  //recorrer un arreglo
+  let sinAgua = planetas.filter(planeta => planeta.volumenAgua<=0 ); return sinAgua
+}
+console.log(`Los planetas sin agua son: `,sinAgua(planetas));
