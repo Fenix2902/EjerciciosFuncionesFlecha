763872842e38a117368129d1cb8f86fa339b4aea
Fenix2902 <alexandercalleagudelo@gmail.com>
2023-09-04 22:08:27 -0500

    PA 04/09/2023 Se realiza el punto cuatro

--- puntocuatro.js
+++ puntocuatro.js
@@ -3,3 +3,20 @@
 // negativa en Joules.
 // Ejemplo: para la entrada {2,4,-8,5,-6} la cantidad de sables
 // defectuosos con energía negativa fue de 2 sables.
+
+let sables = [2, 4, -8, 5, -6,-98,-9];
+
+function conteoEnergiaPositivas(sables) {
+    let positivos = sables.filter(function (sable) {
+         return sable > 0
+    })
+    return positivos.length;
+}
+console.log(`La cantidad de sables Buenos es ${conteoEnergiaPositivas(sables)}`);
+
+//Función flecha
+
+let conteoEnergiaNegativa=(sables)=>{
+    let negativos = sables.filter((sable)=>sable<0);return negativos.length
+}
+console.log(`La cantidad de sables defectuosos es ${conteoEnergiaNegativa(sables)}`);
